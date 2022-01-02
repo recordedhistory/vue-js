f60ddfdcedb1242b3587e48d746ddd9dfc7d9558
Walker Lyle <walker@Walkers-Air.attlocal.net>
2022-01-02 14:11:40 -0600

    form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+  <form>
+       <label>Name:</label>
+      <input type="text" required v-model="name">
+
+      <label>Email:</label>
+      <input type="email" required v-model="email">
+
+        <label>Category:</label>
+      <select v-model="category">
+          <option value="oxfords">Oxfords</option>
+          <option value="brogues">Brogues</option>
+      </select>
+
+  </form>
+
+  <p>Name: {{ name }}</p>
+  <p>Email: {{ email }}</p>
+  <p>Category : {{ category }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            email: '',
+            category: ''
+        }
+    }
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="contact">
     <h1>Contact</h1>
+    <ContactForm />
   </div>
 </template>
+
+
+<script>
+import ContactForm from '@/components/ContactForm.vue'
+
+export default {
+    name: 'Contact',
+    components: { ContactForm }
+}
+
+</script>
